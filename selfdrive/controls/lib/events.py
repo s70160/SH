12f9250f809e09e417ca961f2b19844622f348ad
s70160 <67191334+s70160@users.noreply.github.com>
2020-12-07 09:23:25 +0800

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -548,9 +548,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
     ET.PERMANENT: NormalPermanentAlert("Fan Malfunction", "Contact Support"),
   },
 
-  EventName.cameraMalfunction: {
-    ET.PERMANENT: NormalPermanentAlert("Camera Malfunction", "Contact Support"),
-  },
+  
   # ********** events that affect controls state transitions **********
 
   EventName.pcmEnable: {
